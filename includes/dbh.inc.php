be67ef7152d1d59d1d64801600bfb370432485aa
stvn08 <167376819+stvn08@users.noreply.github.com>
2024-04-18 21:42:19 -0400

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,9 +4,9 @@ $database = getenv('MYSQL_DATABASE');
 $username = getenv('MYSQL_USER');
 $password = getenv('MYSQL_PASSWORD');
 
-$conexion = mysqli_connect($host, $username, $password, $database);
+$conn = mysqli_connect($host, $username, $password, $database);
 
-if (!$conexion) {
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
